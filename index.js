95eb04b8b0181a61ce43de88d448727e0073876d
ComputerScienceSky <78038241+ComputerScienceSky@users.noreply.github.com>
2023-06-18 22:24:50 -0400

    pacman place holder object renders correctly and boundary collision

--- index.js
+++ index.js
@@ -21,6 +21,45 @@ class Boundary{
     }
 }
 
+class Player {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+        this.radius = 15
+    }
+
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'yellow'
+        c.fill()
+        c.closePath()
+    }
+
+    update () {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+ }
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+}
+
+let lastKey = ''
+
 const map = [
     ['-', '-', '-', '-', '-', '-'],
     ['-', ' ', ' ', ' ', ' ', '-'],
@@ -29,6 +68,17 @@ const map = [
     ['-', '-', '-', '-', '-', '-']
 ]
 const boundaries = []
+
+const player = new Player({
+    position:{
+        x: Boundary.width + Boundary.width / 2,
+        y: Boundary.height + Boundary.height / 2
+    },
+    velocity:{
+        x:0,
+        y:0
+    }
+})
 
 map.forEach((row, i) => {
     row.forEach((symbol, j) =>{
@@ -48,6 +98,75 @@ map.forEach((row, i) => {
     })
 })
 
-boundaries.forEach(boundary => {
-    boundary.draw()
-})
+function animate() {
+    requestAnimationFrame(animate)
+    c.clearRect(0, 0, canvas.width, canvas.height)
+    boundaries.forEach(boundary => {
+        boundary.draw()
+
+        if (player.position.y - player.radius <= boundary.position.y + boundary.height
+            && player.position.x + player.radius >= boundary.position.x 
+            && player.position.y + player.radius >= boundary.position.y
+            && player.position.x - player.radius <= boundary.position.x + boundary.width) {
+                console.log('we are colliding')
+        }
+    })
+
+    player.update()
+    player.velocity.y = 0
+    player.velocity.x = 0
+
+    if (keys.w.pressed && lastKey === 'w') {
+        player.velocity.y = -5
+    } else if (keys.a.pressed && lastKey === 'a') {
+        player.velocity.x = -5
+    } else if (keys.s.pressed && lastKey === 's') {
+        player.velocity.y = 5
+    } else if (keys.d.pressed && lastKey === 'd') {
+        player.velocity.x = 5
+    }
+}
+
+animate()
+
+
+
+addEventListener('keydown', ({key}) => {
+    switch (key) {
+        case 'w' :
+            keys.w.pressed = true
+            lastKey = 'w'
+            break
+        case 'a' :
+            keys.a.pressed = true
+            lastKey = 'a'
+            break
+        case 's' :
+            keys.s.pressed = true
+            lastKey = 's'
+            break
+        case 'd' :
+            keys.d.pressed = true
+            lastKey = 'd'
+            break
+    }
+
+}) 
+
+addEventListener('keyup', ({key}) => {
+    switch (key) {
+        case 'w' :
+            keys.w.pressed = false
+            break
+        case 'a' :
+            keys.a.pressed = false
+            break
+        case 's' :
+            keys.s.pressed = false
+            break
+        case 'd' :
+            keys.d.pressed = false
+            break
+    }
+
+}) 
